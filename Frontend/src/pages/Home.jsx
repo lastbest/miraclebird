b6cf9ad3a2d6f85e6963dddb01074191c378b547
jh <jinju0405@naver.com>
2022-09-14 17:29:38 +0900

    [FE][FEAT][B-01] 홈 페이지 캐로쉘 구현

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from "./Home.module.css";
+import { BrowserView, MobileView } from 'react-device-detect';
+import HomeCarousel from "../components/carousel/HomeCarousel";
 
 function Home() {
   return (
-    <div className="App">
-      <div className={styles.content}>The early bird buys the land</div>
-    </div>
+    <>
+    <MobileView>
+      <div className="App">
+        <HomeCarousel></HomeCarousel>
+        <div className={styles.content}>The early bird buys the land</div>
+      </div>
+    </MobileView>
+    <BrowserView>
+          <div className="App">
+        <div className={styles.content}>The early bird buys the land</div>
+      </div>
+    </BrowserView>
+    </>
   );
 }
 
