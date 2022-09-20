2bc08f0f503c0426c3cc01f655a8b89b3fe6f664
Jaecheol Jeong <paneko3@gmail.com>
2022-09-20 15:12:49 +0900

    [FEAT][C-05] 랜드마크 그래프 구현

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
-import { BrowserView, MobileView } from 'react-device-detect';
+import { BrowserView, MobileView } from "react-device-detect";
 import HomeCarousel from "../components/carousel/HomeCarousel";
 import WebCarousel from "../components/carousel/HomeWebCarousel";
 import Rank from "../components/common/Rank";
@@ -8,19 +8,18 @@ import Rank from "../components/common/Rank";
 function Home() {
   return (
     <>
-    <BrowserView>
-      <div className="App">
-        <WebCarousel className={styles.carousel} />
-        <Rank />
-      </div>
-    </BrowserView>
-    <MobileView>
-      <div className="App">
-        <WebCarousel className={styles.carousel} />
-        <Rank />
-      </div>
-    </MobileView>
-  
+      <BrowserView>
+        <div className="App">
+          <WebCarousel className={styles.carousel} />
+          <Rank />
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className="App">
+          <WebCarousel className={styles.carousel} />
+          <Rank />
+        </div>
+      </MobileView>
     </>
   );
 }
