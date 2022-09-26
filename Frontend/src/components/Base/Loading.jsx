1d87fbe3800d12d1ae71ec2d142d61e037378bf9
DESKTOP-KVCQHCD\multicampus <paneko3@gmail.com>
2022-09-26 09:32:42 +0900

    [FIX]API 수정

--- Frontend/src/components/Base/Loading.jsx
+++ Frontend/src/components/Base/Loading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Loading.module.css";
+import spinner from "../../assets/icon/Spinner.gif"
 export const Loading = () => {
   return (
     <div className={styles.background}>
       <div className={styles.loadingText}>잠시만 기다려 주세요.</div>
-      <img src="src/assets/icon/Spinner.gif" width="20%" />
+      <img src={spinner} width="20%" />
     </div>
   );
 };
