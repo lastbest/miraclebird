ab14d40ddce61a87525d21db124929bfeb9a5325
Jaecheol Jeong <paneko3@gmail.com>
2022-09-27 16:33:50 +0900

    [FIX]카메라 수정

--- Frontend/src/pages/Camera.jsx
+++ Frontend/src/pages/Camera.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import styles from "./Camera.module.css";
 import html2canvas from "html2canvas";
 import axios from "axios";
-import Modal from "react-bootstrap/Modal"
-import "./Camera.css"
+import Modal from "react-bootstrap/Modal";
+import "./Camera.css";
 
 function Camera() {
   const [them, setThem] = useState();
@@ -52,6 +52,10 @@ function Camera() {
     });
   }
 
+  useEffect(() => {
+    takepicture();
+  }, []);
+
   function savepicture() {
     axios({
       url: "https://j7c107.p.ssafy.io/image/upload",
@@ -84,61 +88,91 @@ function Camera() {
 
   return (
     <>
-    <div>
-      {url != null ? (
-        <div className={styles.headerCt}>
-          <img
-          className={styles.back}
-          src="src/assets/icon/back_icon.png"
-          onClick={() => {
-            setUrl(null);
-          }}></img>
-          <div className={styles.headerText}>카메라</div>
-        </div>
-        
-      ) : (
-        <></>
-      )}
-      <div className={styles.shotDiv}>
-        {url ? (
-          <div id="screenshot_wrap" className={styles.screenshot_wrap}>
-            <div className={styles.screenshot}>
-              <img src={url} alt="Screenshot" />
-            </div>
-            <div className={styles.watermark}>MIRACLE BIRD</div>
-            <div className={styles.timestamp}>
-              <p>2022-09-22 11:35</p>
-            </div>
-            <div >
-              {them === 1 ? <div className={styles.themText1}>#미라클 모닝</div> : ""}
-              {them === 2 ? <div className={styles.themText2}>#운동</div> : ""}
-              {them === 3 ? <div className={styles.themText3}>#헬스</div> : ""}
-            </div>
-          </div>
-        ) : (
-          <>
+      <div>
+        {url != null ? (
           <div className={styles.headerCt}>
-            <img className={styles.backIcon} src="src/assets/icon/back_icon.png" onClick={()=>(history.back())}></img>
+            <img
+              className={styles.back}
+              src="src/assets/icon/back_icon.png"
+              onClick={() => {
+                setUrl(null);
+              }}></img>
             <div className={styles.headerText}>카메라</div>
           </div>
-          <Webcam
-            audio={false}
-            className={styles.Camera}
-            screenshotFormat="image/png"
-            ref={webcamRef}
-            videoConstraints={videoConstraints}
-          />
-          <div className={styles.cateText}>카테고리를 선택해주세요.</div>
-          <div className={styles.btnCt}>
-            <button className={`mmBtn ${them === 1 ? 'active1' : ''}`} onClick={()=>(setThem(1))}>미라클모닝</button>
-            <button className={`healthBtn ${them === 2 ? 'active2' : ''}`} onClick={()=>(setThem(2))}>운동</button>
-            <button className={`studyBtn ${them === 3 ? 'active3' : ''}`} onClick={()=>(setThem(3))}>스터디</button>
-          </div>
-          </>
+        ) : (
+          <></>
         )}
-      </div>
-
-      
+        <div className={styles.shotDiv}>
+          {url ? (
+            <div id="screenshot_wrap" className={styles.screenshot_wrap}>
+              <div className={styles.screenshot}>
+                <img src={url} alt="Screenshot" />
+              </div>
+              <div className={styles.watermark}>MIRACLE BIRD</div>
+              <div className={styles.timestamp}>
+                <p>
+                  {new Date().getFullYear()}-
+                  {("0" + (new Date().getMonth() + 1)).slice(-2)}-
+                  {("0" + (new Date().getDate() + 1)).slice(-2) + " "}
+                  {("0" + new Date().getHours()).slice(-2)}:
+                  {("0" + new Date().getMinutes()).slice(-2)}
+                </p>
+              </div>
+              <div>
+                {them === 1 ? (
+                  <div className={styles.themText1}>#미라클 모닝</div>
+                ) : (
+                  ""
+                )}
+                {them === 2 ? (
+                  <div className={styles.themText2}>#운동</div>
+                ) : (
+                  ""
+                )}
+                {them === 3 ? (
+                  <div className={styles.themText3}>#스터디</div>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className={styles.headerCt}>
+                <img
+                  className={styles.backIcon}
+                  src="src/assets/icon/back_icon.png"
+                  onClick={() => history.back()}></img>
+                <div className={styles.headerText}>카메라</div>
+              </div>
+              <Webcam
+                audio={false}
+                className={styles.Camera}
+                screenshotFormat="image/png"
+                ref={webcamRef}
+                videoConstraints={videoConstraints}
+              />
+              <div className={styles.cateText}>카테고리를 선택해주세요.</div>
+              <div className={styles.btnCt}>
+                <button
+                  className={`mmBtn ${them === 1 ? "active1" : ""}`}
+                  onClick={() => setThem(1)}>
+                  미라클모닝
+                </button>
+                <button
+                  className={`healthBtn ${them === 2 ? "active2" : ""}`}
+                  onClick={() => setThem(2)}>
+                  운동
+                </button>
+                <button
+                  className={`studyBtn ${them === 3 ? "active3" : ""}`}
+                  onClick={() => setThem(3)}>
+                  스터디
+                </button>
+              </div>
+            </>
+          )}
+        </div>
 
         <div className={styles.camera_footer}>
           {url == null ? (
@@ -152,6 +186,7 @@ function Camera() {
                   } else {
                     capture();
                   }
+                  setTimeout(() => takepicture(), 10);
                 }}></img>
             </div>
           ) : (
@@ -161,7 +196,6 @@ function Camera() {
                   className={styles.shot}
                   src="/download.png"
                   onClick={() => {
-                    takepicture();
                     savepicture();
                   }}></img>
               </div>
