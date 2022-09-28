ae2b24bef1229d467c2e31adde8d255b578a3290
김준우 <junwu19@naver.com>
2022-09-28 13:53:41 +0900

    [FEAT][B-03] 챌린지 최다인증 랭킹 동점시 좋아요 순으로 정렬

--- Backend/miraclebird/src/main/java/com/ssafy/miraclebird/repository/VerificationRepository.java
+++ Backend/miraclebird/src/main/java/com/ssafy/miraclebird/repository/VerificationRepository.java
@@ -16,7 +16,7 @@ public interface VerificationRepository extends JpaRepository<Verification,Long>
     List<Verification> findByUserAndRegtimeBetween(User user, LocalDateTime startDate, LocalDateTime endDate);
     List<Verification> findByUser(User user);
 
-    @Query(value = "SELECT u.name FROM verification v join user u on v.user_idx = u.user_idx GROUP BY v.user_idx order by count(*) desc limit 3", nativeQuery = true)    //예전거 list<long> SELECT user_idx FROM verification GROUP BY user_idx order by count(*) desc limit 3
+    @Query(value = "SELECT u.name FROM (select  v.user_idx, COUNT(vl.verification_like_idx) as likes FROM verification v left outer join verification_like vl on v.verification_idx = vl.verification_idx GROUP BY vl.verification_idx) a join user u on a.user_idx = u.user_idx GROUP BY a.user_idx ORDER BY count(a.user_idx) DESC, SUM(likes) DESC limit 3;", nativeQuery = true)    //예전거 @Query(value = "SELECT u.name FROM verification v join user u on v.user_idx = u.user_idx GROUP BY v.user_idx order by count(*) desc limit 3", nativeQuery = true)
     List<String> getRankByCount();
 
     @Query(value = "SELECT u.name FROM verification v join user u on v.user_idx = u.user_idx GROUP BY v.user_idx order by count(*) desc limit 3", nativeQuery = true)
