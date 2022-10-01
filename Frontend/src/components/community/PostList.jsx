e9cd07399504a5cb9c1903ad4643c62a108b7e6f
Jaecheol Jeong <paneko3@gmail.com>
2022-10-01 10:25:01 +0900

    [FIX] 커뮤니티 수정

--- Frontend/src/components/community/PostList.jsx
+++ Frontend/src/components/community/PostList.jsx
@@ -1,86 +1,88 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import CommonTable from "./CommonTable";
-import CommonTableColumn from "./CommonTableColumn";
-import CommonTableRow from "./CommonTableRow";
-import { postList } from "./PostData";
 
 import styles from "./PostList.module.css";
 import { Link, useNavigate } from "react-router-dom";
 
 const PostList = (props) => {
   const [dataList, setDataList] = useState([]);
+  const [dataMap, setDataMap] = useState([]);
+  var navigate = useNavigate();
   useEffect(() => {
     setDataList(props.postData);
     console.log(props.postData);
   }, [props.postData]);
-  var navigate = useNavigate();
-  let result = [];
-  for (var i = 0; i < props.postData.length; i++) {
-    var item = props.postData[i];
-    result.push(
-      <div
-        onClick={() => {
-          console.log("click");
-          navigate("/community/" + item.postIdx);
-        }}>
-        <CommonTableRow key={item}>
-          <div className={styles.tableRow}>
-            {item.userRole == "ADMIN" ? (
-              <div className={styles.title}>
-                <Link
-                  to={`/community/${item.postIdx}`}
-                  className={styles.titletext2}>
-                  [공지사항] {item.title}
-                </Link>
-              </div>
-            ) : (
-              <div
-                className={styles.title}
-                onClick={() => {
-                  console.log("click");
-                  document.location.href = "/community/" + item.postIdx;
-                }}>
-                <Link
-                  to={`/community/${item.postIdx}`}
-                  className={styles.titletext}>
-                  {item.title}
-                </Link>
-              </div>
-            )}
-            <div className={styles.readCount}>
-              <img src="/view.png" alt="view" className={styles.viewicon} />
-              <div className={styles.counttext}>{item.hit}</div>
-            </div>
-          </div>
-          <div className={styles.tableRow2}>
-            <div className={styles.datename}>
-              <div className={styles.createDate}>
-                {item.regtime[0]}-{item.regtime[1]}-{item.regtime[2]}
-              </div>
-            </div>
-            <div className={styles.nickname}>
-              {item.image_url == null ? (
-                <img alt="profile" src="src/assets/icon/profile_default.jpg" />
+  useEffect(() => {
+    let result = [];
+    for (var i = 0; i < props.postData.length; i++) {
+      const item = props.postData[i];
+      result.push(
+        <tr
+          className={styles.tablerow}
+          key={item.postIdx}
+          onClick={() => {
+            navigate("/community/" + item.postIdx);
+          }}>
+          <td>
+            <div className={styles.tableRow}>
+              {item.userRole == "ADMIN" ? (
+                <div className={styles.title}>
+                  <Link
+                    to={`/community/${item.postIdx}`}
+                    className={styles.titletext2}>
+                    [공지사항] {item.title}
+                  </Link>
+                </div>
               ) : (
-                <img alt="profile" src={item.image_url} />
+                <div
+                  className={styles.title}
+                  onClick={() => {
+                    document.location.href = "/community/" + item.postIdx;
+                  }}>
+                  <Link
+                    to={`/community/${item.postIdx}`}
+                    className={styles.titletext}>
+                    {item.title}
+                  </Link>
+                </div>
               )}
-
-              {item.name}
+              <div className={styles.readCount}>
+                <img src="/view.png" alt="view" className={styles.viewicon} />
+                <div className={styles.counttext}>{item.hit}</div>
+              </div>
             </div>
-            {/* <CommonTableColumn className={styles.no}>#{ item.no }</CommonTableColumn> */}
-          </div>
-        </CommonTableRow>
-      </div>
-    );
-  }
+            <div className={styles.tableRow2}>
+              <div className={styles.datename}>
+                <div className={styles.createDate}>
+                  {item.regtime[0]}-{item.regtime[1]}-{item.regtime[2]}
+                </div>
+              </div>
+              <div className={styles.nickname}>
+                {item.image_url == null ? (
+                  <img
+                    alt="profile"
+                    src="src/assets/icon/profile_default.jpg"
+                  />
+                ) : (
+                  <img alt="profile" src={item.image_url} />
+                )}
+
+                {item.name}
+              </div>
+            </div>
+          </td>
+        </tr>
+      );
+    }
+    setDataMap(result);
+  }, []);
+
   return (
     <>
       <div className={styles.tableContainer}>
-        <CommonTable headersName={[]}>
-          {/* <CommonTable headersName={['No', '제목', '날짜', '♡']}> */}
-          {result}
-        </CommonTable>
+        <table className={styles.commontable}>
+          <tbody>{dataMap}</tbody>
+        </table>
       </div>
     </>
   );
