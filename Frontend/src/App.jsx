d45210f5624a4e9f0bdc7445d88c11add2336549
Jaecheol Jeong <paneko3@gmail.com>
2022-10-02 23:06:25 +0900

    [FEAT] NotFound 페이지 구현

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,94 +25,109 @@ import Deposit from "./pages/Deposit";
 import HealthFeed from "./components/Feed/HealthFeed";
 import StudyFeed from "./components/Feed/StudyFeed";
 import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 import { BrowserView, MobileView } from "react-device-detect";
 
 function App() {
   return (
     <>
-    <div className={styles.App}>
-      <BrowserRouter>
-        <Header className={styles.Header} />
-        <BrowserView>
-        <div className={styles.Contents2}>
-          <Routes>
-            {/* <Route path="/*" element={<Store />}></Route> */}
-            <Route path="/" element={<Home />}></Route>
-            <Route
-              path="/oauth2/redirect"
-              element={<OAuth2RedirectHandler />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/store" element={<Store />}></Route>
-            <Route path="/landmark" element={<Landmark />}></Route>
-            <Route path="/camera" element={<Camera />}></Route>
-            <Route path="/challenge" element={<Challenge />}></Route>
-            <Route path="/community" element={<Community />}></Route>
-            <Route path="/community/create" element={<CreatePost />}></Route>
-            <Route
-              path="/community/update/:postIdx"
-              element={<UpdatePost />}></Route>
-            <Route path="/community/:postIdx" element={<PostView />}></Route>
-            <Route
-              path="/challenge/morning"
-              element={<MiracleMorning />}></Route>
-            <Route path="/challenge/health" element={<Health />}></Route>
-            <Route path="/challenge/study" element={<Study />}></Route>
-            <Route path="/challenge/deposit" element={<Deposit />}></Route>
-            <Route
-              path="/challenge/morning/feed"
-              element={<MiracleFeed />}></Route>
-            <Route
-              path="/challenge/health/feed"
-              element={<HealthFeed />}></Route>
-            <Route path="/challenge/study/feed" element={<StudyFeed />}></Route>
-            <Route path="/mypage" element={<MyPage />}></Route>
-            <Route path="/reinforce" element={<Reinforce />}></Route>
-            <Route path="/admin" element={<Admin />}></Route>
-          </Routes>
-        </div>
-        </BrowserView>
-        <MobileView>
-          
-        <div className={styles.Contents}>
-          <Routes>
-            {/* <Route path="/*" element={<Store />}></Route> */}
-            <Route path="/" element={<Home />}></Route>
-            <Route
-              path="/oauth2/redirect"
-              element={<OAuth2RedirectHandler />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/store" element={<Store />}></Route>
-            <Route path="/landmark" element={<Landmark />}></Route>
-            <Route path="/camera" element={<Camera />}></Route>
-            <Route path="/challenge" element={<Challenge />}></Route>
-            <Route path="/community" element={<Community />}></Route>
-            <Route path="/community/create" element={<CreatePost />}></Route>
-            <Route
-              path="/community/update/:postIdx"
-              element={<UpdatePost />}></Route>
-            <Route path="/community/:postIdx" element={<PostView />}></Route>
-            <Route
-              path="/challenge/morning"
-              element={<MiracleMorning />}></Route>
-            <Route path="/challenge/health" element={<Health />}></Route>
-            <Route path="/challenge/study" element={<Study />}></Route>
-            <Route path="/challenge/deposit" element={<Deposit />}></Route>
-            <Route
-              path="/challenge/morning/feed"
-              element={<MiracleFeed />}></Route>
-            <Route
-              path="/challenge/health/feed"
-              element={<HealthFeed />}></Route>
-            <Route path="/challenge/study/feed" element={<StudyFeed />}></Route>
-            <Route path="/mypage" element={<MyPage />}></Route>
-            <Route path="/reinforce" element={<Reinforce />}></Route>
-            <Route path="/admin" element={<Admin />}></Route>
-          </Routes>
-        </div>
-        </MobileView>
-        <Footer className={styles.Footer} />
-      </BrowserRouter>
-    </div>
+      <div className={styles.App}>
+        <BrowserRouter>
+          <Header className={styles.Header} />
+          <BrowserView>
+            <div className={styles.Contents2}>
+              <Routes>
+                {/* <Route path="/*" element={<Store />}></Route> */}
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+                <Route
+                  path="/oauth2/redirect"
+                  element={<OAuth2RedirectHandler />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/store" element={<Store />}></Route>
+                <Route path="/landmark" element={<Landmark />}></Route>
+                <Route path="/camera" element={<Camera />}></Route>
+                <Route path="/challenge" element={<Challenge />}></Route>
+                <Route path="/community" element={<Community />}></Route>
+                <Route
+                  path="/community/create"
+                  element={<CreatePost />}></Route>
+                <Route
+                  path="/community/update/:postIdx"
+                  element={<UpdatePost />}></Route>
+                <Route
+                  path="/community/:postIdx"
+                  element={<PostView />}></Route>
+                <Route
+                  path="/challenge/morning"
+                  element={<MiracleMorning />}></Route>
+                <Route path="/challenge/health" element={<Health />}></Route>
+                <Route path="/challenge/study" element={<Study />}></Route>
+                <Route path="/challenge/deposit" element={<Deposit />}></Route>
+                <Route
+                  path="/challenge/morning/feed"
+                  element={<MiracleFeed />}></Route>
+                <Route
+                  path="/challenge/health/feed"
+                  element={<HealthFeed />}></Route>
+                <Route
+                  path="/challenge/study/feed"
+                  element={<StudyFeed />}></Route>
+                <Route path="/mypage" element={<MyPage />}></Route>
+                <Route path="/reinforce" element={<Reinforce />}></Route>
+                <Route path="/admin" element={<Admin />}></Route>
+              </Routes>
+            </div>
+          </BrowserView>
+          <MobileView>
+            <div className={styles.Contents}>
+              <Routes>
+                {/* <Route path="/*" element={<Store />}></Route> */}
+                <Route path="/" element={<Home />}></Route>
+
+                <Route path="/*" element={<NotFound />}></Route>
+                <Route
+                  path="/oauth2/redirect"
+                  element={<OAuth2RedirectHandler />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/store" element={<Store />}></Route>
+                <Route path="/landmark" element={<Landmark />}></Route>
+                <Route path="/camera" element={<Camera />}></Route>
+                <Route path="/challenge" element={<Challenge />}></Route>
+                <Route path="/community" element={<Community />}></Route>
+                <Route
+                  path="/community/create"
+                  element={<CreatePost />}></Route>
+                <Route
+                  path="/community/update/:postIdx"
+                  element={<UpdatePost />}></Route>
+                <Route
+                  path="/community/:postIdx"
+                  element={<PostView />}></Route>
+                <Route
+                  path="/challenge/morning"
+                  element={<MiracleMorning />}></Route>
+                <Route path="/challenge/health" element={<Health />}></Route>
+                <Route path="/challenge/study" element={<Study />}></Route>
+                <Route path="/challenge/deposit" element={<Deposit />}></Route>
+                <Route
+                  path="/challenge/morning/feed"
+                  element={<MiracleFeed />}></Route>
+                <Route
+                  path="/challenge/health/feed"
+                  element={<HealthFeed />}></Route>
+                <Route
+                  path="/challenge/study/feed"
+                  element={<StudyFeed />}></Route>
+                <Route path="/mypage" element={<MyPage />}></Route>
+                <Route path="/reinforce" element={<Reinforce />}></Route>
+                <Route path="/admin" element={<Admin />}></Route>
+              </Routes>
+            </div>
+          </MobileView>
+          <Footer className={styles.Footer} />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
