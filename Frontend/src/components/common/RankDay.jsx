08a5ea922e7c9fd6cdcf549157cc8cb5d77a4059
Jaecheol Jeong <paneko3@gmail.com>
2022-10-03 22:30:20 +0900

    [FIX] 디자인 수정

--- Frontend/src/components/common/RankDay.jsx
+++ Frontend/src/components/common/RankDay.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
 import styles from "./RankDay.module.css";
 import { NOW_ACCESS_TOKEN, API_BASE_URL } from "/src/constants";
-import { BrowserView, MobileView } from "react-device-detect";
 
 function RankDay() {
   const [nickname, setNickname] = useState([]);
 
   const mainApi = async () => {
     try {
-      const response = await fetch(
-        API_BASE_URL + "/verification/ranking/streak",
-        {
-          method: "GET",
-        }
-      );
+      const response = await fetch(API_BASE_URL + "/verification/ranking/", {
+        method: "GET",
+      });
       const rankingresult = await response.json();
       setNickname(rankingresult);
     } catch (error) {
@@ -25,84 +21,24 @@ function RankDay() {
   }, []);
   return (
     <>
-      <BrowserView>
-        <div className={styles.medalcontainer}>
-          <div>
-            <img
-              src="src/assets/icon/silver.png"
-              className={styles.silverImg}></img>
-          </div>
-          <div>
-            <img
-              src="src/assets/icon/gold.png"
-              className={styles.goldImg}></img>
-          </div>
-          <div>
-            <img
-              src="src/assets/icon/bronze.png"
-              className={styles.bronzeImg}></img>
-          </div>
-
-          <img className={styles.goldImg}></img>
-          <img className={styles.silverImg}></img>
-          <img className={styles.bronzeImg}></img>
-          <div className={styles.silver}>{nickname[1]}</div>
-
-          <div className={styles.gold}>{nickname[0]}</div>
-          <div className={styles.bronze}>{nickname[2]}</div>
-
-          <img
-            className={styles.rankingImg}
-            alt="rank"
-            src="src/assets/icon/ranking.png"></img>
+      <div className={styles.medalcontainer}>
+        <div></div>
+        <div className={styles.gold}>
+          <img alt="gold" src="./gold.png" />
+          <p className={styles.slidetext}>{nickname[0]}</p>
         </div>
-      </BrowserView>
-      <MobileView>
-        <div className={styles.medalcontainermobile}>
-          <div>
-            <img
-              src="src/assets/icon/silver.png"
-              className={styles.silverImg}></img>
-          </div>
-          <div>
-            <img
-              src="src/assets/icon/gold.png"
-              className={styles.goldImg}></img>
-          </div>
-          <div>
-            <img
-              src="src/assets/icon/bronze.png"
-              className={styles.bronzeImg}></img>
-          </div>
 
-          <img className={styles.goldImg}></img>
-          <img className={styles.silverImg}></img>
-          <img className={styles.bronzeImg}></img>
-          <div className={styles.silver}>{nickname[1]}</div>
-
-          <div className={styles.gold}>{nickname[0]}</div>
-          <div className={styles.bronze}>{nickname[2]}</div>
-
-          <img
-            className={styles.rankingImg}
-            alt="rank"
-            src="src/assets/icon/ranking.png"></img>
+        <div></div>
+        <div className={styles.silver}>
+          <img alt="silver" src="./silver.png" />
+          <p className={styles.slidetext}>{nickname[1]}</p>
         </div>
-      </MobileView>
-      {/* <div>
-          <div className={styles.silver}>
-            <img alt="silver" src="./silver.png" />
-            <p className={styles.slidetext}>{nickname[1]}</p>
-          </div>
-          <div className={styles.gold}>
-            <img alt="gold" src="./gold.png" />
-            <p className={styles.slidetext}>{nickname[0]}</p>
-          </div>
-          <div className={styles.bronze}>
-            <img alt="bronze" src="./bronze.png" />
-            <p className={styles.slidetext}>{nickname[2]}</p>
-          </div>
-        </div> */}
+        <div></div>
+        <div className={styles.bronze}>
+          <img alt="bronze" src="./bronze.png" />
+          <p className={styles.slidetext}>{nickname[2]}</p>
+        </div>
+      </div>
     </>
   );
 }
