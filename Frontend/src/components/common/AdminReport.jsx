3e66faf64062e1a429a593c91b8ce8b8fb16c1bb
Jaecheol Jeong <paneko3@gmail.com>
2022-10-04 14:14:09 +0900

    [FIX] 관리자 페이지 수정

--- Frontend/src/components/common/AdminReport.jsx
+++ Frontend/src/components/common/AdminReport.jsx
@@ -5,23 +5,22 @@ import axios from "axios";
 import { NOW_ACCESS_TOKEN, API_BASE_URL } from "/src/constants";
 
 function AdminReport({ reportData }) {
+  const [verificationIdx, setVerificationIdx] = useState("");
   const [reportIdx, setReportIdx] = useState("");
-  const [reportDetail, setReportDetail] = useState();
   const [reportContent, setReportContent] = useState("");
-
+  const [reportDetail, setReportDetail] = useState();
   const [reportSelfie, setReportSelfie] = useState();
   const [reportName, setReportName] = useState();
   const [reportUserIdx, setReportUserIdx] = useState();
-
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   useEffect(() => {
-    console.log(reportIdx);
+    console.log(verificationIdx);
     axios({
-      url: API_BASE_URL + "/verification/" + reportIdx,
+      url: API_BASE_URL + "/verification/" + verificationIdx,
       method: "GET",
       headers: {
         Authorization: "Bearer " + NOW_ACCESS_TOKEN,
@@ -33,7 +32,11 @@ function AdminReport({ reportData }) {
       setReportName(res.data.name);
       setReportUserIdx(res.data.userIdx);
     });
-  }, [reportIdx]);
+  }, [verificationIdx]);
+
+  useEffect(() => {
+    console.log(verificationIdx);
+  }, []);
 
   return (
     <>
@@ -43,7 +46,10 @@ function AdminReport({ reportData }) {
             <div
               className={styles.post}
               onClick={() => {
-                setReportIdx(post.verificationIdx);
+                setReportIdx(post.reportIdx);
+                console.log(post.reportIdx);
+                setReportContent(post.description);
+                setVerificationIdx(post.verificationIdx);
                 setReportContent(post.description);
                 handleShow();
               }}>
@@ -63,21 +69,14 @@ function AdminReport({ reportData }) {
         <Modal.Body className={styles.modalcontent}>
           <img alt="post" src={reportSelfie} className={styles.postImg} />
           <div className={styles.text}>
-            {reportName} {reportUserIdx} {reportIdx}
+            {reportName} {reportContent}
           </div>
           <div className={styles.btnCt}>
             <button
               className={styles.passBtn}
               onClick={() => {
-                console.log(reportIdx);
-              }}>
-              보류
-            </button>
-            <button
-              className={styles.deleteBtn}
-              onClick={() => {
                 axios({
-                  url: API_BASE_URL + "/verification/" + reportIdx,
+                  url: API_BASE_URL + "/report/" + reportIdx,
                   method: "delete",
                   headers: {
                     Authorization: "Bearer " + NOW_ACCESS_TOKEN,
@@ -89,12 +88,72 @@ function AdminReport({ reportData }) {
                   .catch((error) => {
                     console.log(error);
                   });
+                handleClose();
+              }}>
+              보류
+            </button>
+            <button
+              className={styles.deleteBtn}
+              onClick={() => {
+                axios({
+                  url: API_BASE_URL + "/verification/" + verificationIdx,
+                  method: "delete",
+                  headers: {
+                    Authorization: "Bearer " + NOW_ACCESS_TOKEN,
+                  },
+                })
+                  .then((res) => {
+                    console.log(res.data);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+                axios({
+                  url: API_BASE_URL + "/report/" + reportIdx,
+                  method: "delete",
+                  headers: {
+                    Authorization: "Bearer " + NOW_ACCESS_TOKEN,
+                  },
+                })
+                  .then((res) => {
+                    console.log(res.data);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+                handleClose();
               }}>
               삭제
             </button>
             <button
               className={styles.outBtn}
               onClick={() => {
+                axios({
+                  url: API_BASE_URL + "/verification/" + verificationIdx,
+                  method: "delete",
+                  headers: {
+                    Authorization: "Bearer " + NOW_ACCESS_TOKEN,
+                  },
+                })
+                  .then((res) => {
+                    console.log(res.data);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+                axios({
+                  url: API_BASE_URL + "/report/" + reportIdx,
+                  method: "delete",
+                  headers: {
+                    Authorization: "Bearer " + NOW_ACCESS_TOKEN,
+                  },
+                })
+                  .then((res) => {
+                    console.log(res.data);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
                 axios({
                   url: API_BASE_URL + "/user/blacklist",
                   method: "put",
@@ -111,6 +170,7 @@ function AdminReport({ reportData }) {
                   .catch((error) => {
                     console.log(error);
                   });
+                handleClose();
               }}>
               블랙리스트 등록
             </button>
