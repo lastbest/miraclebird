6de48216c3afdcc53502c8a0df6f9e2ac23bec40
DESKTOP-KVCQHCD\multicampus <paneko3@gmail.com>
2022-10-05 15:40:36 +0900

    [FIX]API 변경

--- Frontend/src/components/community/PostList.jsx
+++ Frontend/src/components/community/PostList.jsx
@@ -54,7 +54,7 @@ const PostList = (props) => {
             <div className={styles.tableRow2}>
               <div className={styles.datename}>
                 <div className={styles.createDate}>
-                  {item.regtime[0]}-{item.regtime[1]}-{'0'+item.regtime[2]}
+                  {item.regtime[0]}-{item.regtime[1]}-{item.regtime[2]}
                 </div>
               </div>
               <div className={styles.nickname}>
